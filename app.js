94366a03e6f2d271d71b0c35be45311820b7355e
Thalissa Jenniffer <thalissajenniffer@hotmail.com>
2018-08-09 17:10:07 -0300

    barra de xp

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ $(document).ready(function() {
   $('.splash-content').delay('4000').fadeOut('slow');
   $('#splash').delay('6000').fadeOut('slow');
 
-  
   // $(".includePoints").click(function(e))
   // e.preventDefault();
 
+  //barra de xp
+const levelOne = 30;
+const levelTwo = 50;
+const levelThree = 70;
+const levelFour = 100;
+function levelPoints (totalPoints){
+  var levelAtual = 0;
+  var changeLevel = $("#levelNivel");
+  if (totalPoints >= levelOne){
+    changeLevel.html("1");
+  }
+}
 
 
 
 
-    
-  
-  
+
+
+
 
 
 
