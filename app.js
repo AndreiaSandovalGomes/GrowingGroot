c92bfc6853c136e1ab2c4b3c5ec5618b5b505477
Dine <ariadne0288@hotmail.com>
2018-08-09 17:14:37 -0300

    app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $('.title-splash').delay('1000').fadeIn('slow');
   $('.gif-splash').delay('2000').fadeIn('slow');
-  $('.splash-content').delay('4000').fadeOut('slow');
-  $('#splash').delay('6000').fadeOut('slow');
+  $('.splash-content').delay('5000').fadeOut('slow');
+  $('.splash').delay('6000').fadeOut('slow');
+
 
   
   // $(".includePoints").click(function(e))
