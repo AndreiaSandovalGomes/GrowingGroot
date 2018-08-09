8965565530902cb027cba8e159f9e0749757f7dd
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-08-09 12:11:47 -0300

    incluido login

---
+++ app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $(".includePoints").click(function(e))
+  e.preventDefault();
+  })
---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $(".sign-up-button").click(function(e){
+    e.preventDefault();
+    var email = $("sign-up-email").val();
+    var password = $("sign-up-password").val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(function(){
+        window.location = "app.html";
+      })
+      .catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+      });
+    })
+    $(".sign-in-button").click(function(e){
+      e.preventDefault();
+      var email = $("sign-in-email").val();
+      var password = $("sign-in-password").val();
+
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(){
+        })
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert(errorMessage);
+        });
+      })
+  })
