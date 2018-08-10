e248b187c3d9d9544c727ddb06f8c5f6fcc9d529
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-08-10 10:20:25 -0300

    firebase funcionando

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var database = firebase.database();
-// var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
-// console.log(USER_ID);
+var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
 $(document).ready(function() {
   $('.title-splash').delay('1000').fadeIn('slow');
@@ -8,48 +7,47 @@ $(document).ready(function() {
   $('.splash-content').delay('5000').fadeOut('slow');
   $('.splash').delay('6000').fadeOut('slow');
 
-
-  // $(".includePoints").click(function(e))
-  // e.preventDefault();
-
+  getTasksFromDB();
   $(".btnSumPoints").click(addTasksClick);
-
-  });
-  function addTasksClick(event) {
-    event.preventDefault();
-
-    var newTask = $("#points").val();
-    var taskFromDB = addTaskToDB(newTask);
-
-    createListItem(newTask, taskFromDB.key)
-  }
-  function addTaskToDB(text) {
-    return database.ref("Pontuacao" + USER_ID).push({
-      text: text
-    });
-  }
-  function getTasksFromDB() {
-    database.ref("pontuacao" + USER_ID).once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var childKey = childSnapshot.key;
-          var childData = childSnapshot.val();
-          createListItem(childData.text, childKey);
-          console.log("salvo");
-        });
-      });
-  }
-
-  //barra de xp
-  const levelOne = 30;
-  const levelTwo = 50;
-  const levelThree = 70;
-  const levelFour = 100;
-  function levelPoints (totalPoints){
-    var levelAtual = 0;
-    var changeLevel = $("#levelNivel");
-    if (totalPoints >= levelOne){
-      changeLevel.html("1");
-    }
-  }
 });
+
+function addTasksClick(event) {
+  event.preventDefault();
+  var newTask = $("#points").val();
+  var taskFromDB = addTaskToDB(newTask);
+  getTasksFromDB();
+}
+
+function addTaskToDB(text) {
+  return database.ref("points/" + USER_ID).set({
+    sumPoints: text
+  });
+}
+
+function getTasksFromDB() {
+  database.ref("points/" + USER_ID).once('value')
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+    $(".retorno").text(childData);
+    console.log(childData);
+    });
+  });
+}
+function returnItem(text, key) {
+  $(".retorno").text(text);
+}
+
+
+//barra de xp
+const levelOne = 30;
+const levelTwo = 50;
+const levelThree = 70;
+const levelFour = 100;
+function levelPoints (totalPoints){
+  var levelAtual = 0;
+  var changeLevel = $("#levelNivel");
+  if (totalPoints >= levelOne){
+    changeLevel.html("1");
+  }
+}
--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 $(document).ready(function() {
-  $(".sign-up-button").click(function(e){
-    e.preventDefault();
-    var email = $(".sign-email").val();
-    var password = $(".sign-password").val();
+  $(".sign-up-button").click(signUpClick);
+  $(".sign-in-button").click(signInClick);
+});
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(function(response) {
-        // var userId = response.user.uid;
-        // redirectToTasks(userId);
-        window.location = "app.html";
-      })
-      .catch(function(error){
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-      });
+
+function signUpClick(event) {
+  event.preventDefault();
+  var email = $(".sign-email").val();
+  var password = $(".sign-password").val();
+  createUser(email, password);
+}
+
+function createUser(email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      alert("Cadastro concluido com sucesso !!!\n " + email)
     })
-    $(".sign-in-button").click(function(e){
-      e.preventDefault();
-      var email = $(".sign-email").val();
-      var password = $(".sign-password").val();
+    .catch(function(error) {
+      handleError(error);
+    });
+}
 
-      firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function(response) {
-          // var userId = response.user.uid;
-          // redirectToTasks(userId);
-          window.location = "app.html";
-        })
-        .catch(function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          alert(errorMessage);
-        });
-      })
-  })
+function signInClick(event) {
+  event.preventDefault();
+  var email = $(".sign-email").val();
+  var password = $(".sign-password").val();
+  signInUser(email, password);
+}
+
+function signInUser(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(response) {
+      var userId = response.user.uid;
+      redirectToTasks(userId);
+    })
+    .catch(function(error) {
+      handleError(error)
+    });
+}
+
+function handleError(error) {
+  var errorMessage = error.message;
+  alert(errorMessage);
+}
+
+function redirectToTasks(userId) {
+  window.location = "app.html?id=" + userId;
+}
