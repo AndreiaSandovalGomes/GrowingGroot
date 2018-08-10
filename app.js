61ec97490b686f86915cc69e276fe13e056d8882
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-08-09 21:09:38 -0300

    acerto de erros console

--- app.js
+++ app.js
@@ -53,20 +53,20 @@ function levelPoints (totalPoints){
           console.log("salvo");
         });
       });
-  }
+  };
 
-  //barra de xp
-  const levelOne = 30;
-  const levelTwo = 50;
-  const levelThree = 70;
-  const levelFour = 100;
-  function levelPoints (totalPoints){
-    var levelAtual = 0;
-    var changeLevel = $("#levelNivel");
-    if (totalPoints >= levelOne){
-      changeLevel.html("1");
-    }
-  }
+//barra de xp
+  // const levelOne = 30;
+  // const levelTwo = 50;
+  // const levelThree = 70;
+  // const levelFour = 100;
+  // function levelPoints (totalPoints){
+  //   var levelAtual = 0;
+  //   var changeLevel = $("#levelNivel");
+  //   if (totalPoints >= levelOne){
+  //     changeLevel.html("1");
+  //   }
+  // }
 
   $(".btn-category").click(function(e) {
   e.preventDefault();
